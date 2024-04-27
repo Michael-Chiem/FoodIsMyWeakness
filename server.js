5b9e42c472526f2b97963264cbe6a077e48a7e90
Michael Chiem <michaelchiem1@hotmail.com>
2024-04-27 16:26:25 -0600

    Updating the Server.js File

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ app.use(express.json());
 
 app.use(require('./routes'));
 
-mongoose.connect( "mongodb://127.0.0.1:27017/IwantMyMoneyBack",
+mongoose.connect("mongodb://127.0.0.1:27017/IwantMyMoneyBack",
 
-{
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,    
-}
+    {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
 );
 
 mongoose.set("useCreateIndex", true);
+mongoose.set("debug", true);
+
+app.listen(PORT, () => {
+    console.log('App listening on port ${PORT}!!!!!!!!');
+});    
