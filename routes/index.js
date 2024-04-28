dec821c31fc1dced108ed3fa85ff4c8e9d836d8b
Michael Chiem <michaelchiem1@hotmail.com>
2024-04-28 06:54:29 -0600

    Adding Index Route JS file

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('rxpress').Router();
+const apiRoutes = require('./API');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+module.exports = router;
